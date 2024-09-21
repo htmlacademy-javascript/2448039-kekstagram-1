c62dd21d6cd6da0f85755c767466c45e93a13cfa
Daria <dashusik_@mail.ru>
2024-09-21 20:08:19 +0300

    Add create array photos

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
-/*В файле main.js напишите необходимые функции для создания массива из 25 сгенерированных объектов. Каждый объект массива — описание фотографии, опубликованной пользователем.
-Структура каждого объекта должна быть следующей:
-
-id, число — идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться.
-url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
-description, строка — описание фотографии. Описание придумайте самостоятельно.
-likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
-comments, массив объектов — список комментариев, оставленных другими пользователями к этой фотографии. Количество комментариев к каждой фотографии вы определяете на своё усмотрение.
-Все комментарии генерируются случайным образом. Пример описания объекта с комментарием:
-{
-  id: 135,
-  avatar: 'img/avatar-6.svg',
-  message: 'В целом всё неплохо. Но не всё.',
-  name: 'Артём',
-}
-У каждого комментария есть идентификатор — id — любое число. Идентификаторы не должны повторяться.
-Поле avatar — это строка, значение которой формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg. Аватарки подготовлены в директории img.
-Для формирования текста комментария — message — вам необходимо взять одно или два случайных предложения из представленных ниже:
-Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name. */
+const PHOTO_COUNT = 25;
+const LIKE_MIN_COUNT = 15;
+const LIKE_MAX_COUNT = 200;
+const COMMENT_COUNT = 5;
+const AVATAR_COUNT = 6;
 
 const DESCRIPTIONS = [
   'Галечный пляж, окруженный зеленью, с шезлонгами и виднеющимся вдали отелем',
@@ -45,12 +31,14 @@ const DESCRIPTIONS = [
   'Концертный зал, где зрители изображают символ треугольника руками',
   'Белый джип, проезжающий в тропическом лесу по затопленной дороге',
 ];
-
 const MESSAGES = [
   'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-
 const NAMES = [
   'Кекс',
   'Манюня',
@@ -61,19 +49,63 @@ const NAMES = [
   'Шелл'
 ];
 
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
   const result = Math.random() * (upper - lower + 1) + lower;
+
   return Math.floor(result);
 };
 
-const createPhoto = () => {
-  return {
-    idPhoto: 0,
-    urlPhoto: '',
-    descriptionPhoto: '',
-    likesPhoto: 0,
-    commentsPhoto: [],
+const getIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
   };
 };
+const generatePhotoId = getIdGenerator();
+const generateCommentId = getIdGenerator();
+
+const getUniqueRandomId = (min, max) => {
+  const previousValues = [];
+
+  return () => {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    } while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const generateUrl = getUniqueRandomId (1, PHOTO_COUNT);
+const generateAvatar = getUniqueRandomId (1, AVATAR_COUNT);
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(1, elements.length - 1)];
+
+const createMessage = () => Array.from({ length: getRandomInteger(1, 2) }, () => getRandomArrayElement(MESSAGES)).join(' ');
+
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${generateAvatar}.svg.`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES),
+});
+
+
+const createPhoto = () => ({
+  id: generatePhotoId(),
+  url: `photos/${generateUrl}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger (LIKE_MIN_COUNT, LIKE_MAX_COUNT),
+  comments: Array.from({length: getRandomInteger (1, COMMENT_COUNT)}, createComment),
+});
+
+const getArrayPhoto = () => Array.from({length: PHOTO_COUNT}, createPhoto);
+
+getArrayPhoto();
