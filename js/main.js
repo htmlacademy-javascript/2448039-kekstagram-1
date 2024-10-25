2737d9ba1f0a88ee5460b9a99e7fabf1c5b97387
Daria <dashusik_@mail.ru>
2024-10-25 15:20:36 +0300

    Add picture-modal.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {getArrayPhoto} from './data.js';
 import './thumbnail.js';
 import './big-picture.js';
+import './picture-modal.js';
 
 getArrayPhoto();
---
+++ js/picture-modal.js
@@ -0,0 +1,11 @@
+import './thumbnail.js';
+
+const container = document.querySelector('.pictures');
+
+container.addEventListener('click', (evt) => {
+  const thumbnailsElement = evt.target.closet('[data-thumbnail-id]');
+  if (!thumbnailsElement) {
+    return;
+  }
+
+});
