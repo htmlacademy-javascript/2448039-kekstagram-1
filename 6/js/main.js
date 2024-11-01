006025cc2696d6389e79f18f6ff2fa2978044353
Keks <keks@htmlacademy.ru>
2024-11-01 12:27:50 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -1,6 +1,4 @@
 import {getArrayPhoto} from './data.js';
-import './thumbnail.js';
-import './big-picture.js';
 import {renderPictures} from './picture-modal.js';
 
 renderPictures(getArrayPhoto());
