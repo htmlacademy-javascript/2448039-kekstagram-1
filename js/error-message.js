984a5a454298b3032d7137eeec8ff46c4a3dda8f
Daria <dashusik_@mail.ru>
2024-12-05 17:14:32 +0300

    fix error-message.js

--- js/error-message.js
+++ js/error-message.js
@@ -1,41 +1,44 @@
 import { isEscapeKey } from './util.js';
 
 const errorTemplate = document.querySelector('#error').content.querySelector('.error');
+let errorElement;
 
-const onDocumentKeydown = (evt, errorElement) => {
+const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
-    closeErrorMessage(errorElement);
+    closeErrorMessage();
   }
 };
 
-const onErrorButtonClick = (errorElement) => {
-  closeErrorMessage(errorElement);
+const onErrorButtonClick = () => {
+  closeErrorMessage();
 };
 
 const showErrorMessage = () => {
-  const errorElement = errorTemplate.cloneNode(true);
+  if (errorElement) {
+    return;
+  }
+  errorElement = errorTemplate.cloneNode(true);
   document.body.append(errorElement);
 
-  document.addEventListener('keydown', (evt) => {
-    onDocumentKeydown(evt, errorElement);
-  });
+  document.addEventListener('keydown', onDocumentKeydown);
 
   const errorButton = errorElement.querySelector('.error__button');
-  errorButton.addEventListener('click', () => onErrorButtonClick(errorElement));
+  errorButton.addEventListener('click', onErrorButtonClick);
 
   errorElement.addEventListener('click', (evt) => {
     if (evt.target === errorElement) {
-      closeErrorMessage(errorElement);
+      closeErrorMessage();
     }
   });
 };
 
-function closeErrorMessage (errorElement) {
-  errorElement.remove();
-  document.removeEventListener('keydown', (evt) => {
-    onDocumentKeydown(evt, errorElement);
-  });
+function closeErrorMessage () {
+  if (errorElement) {
+    errorElement.remove();
+    errorElement = null;
+  }
+  document.removeEventListener('keydown', onDocumentKeydown);
 }
 
 export {showErrorMessage};
